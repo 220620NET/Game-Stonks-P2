4688fda7ebf0b356a61142370ba07bf5df2e37c1
Elizabeth Kang <liz.kang@hey.com>
2022-07-25 16:54:58 -0500

    Fixed syntax for script

--- DB.sql
+++ DB.sql
@@ -1,6 +1,5 @@
 CREATE SCHEMA P2;
 
-
 CREATE TABLE Users (
 	user_id INT IDENTITY,
 	email VARCHAR(100) UNIQUE NOT NULL,
@@ -14,26 +13,29 @@ CREATE TABLE Profiles (
 	first_name VARCHAR(50) NOT NULL,
 	last_name VARCHAR(50) NOT NULL,
 	display_name VARCHAR(50) NOT NULL,
-	profile_picture VARCHAR(50)
-	PRIMARY KEY(profile_id)
+	profile_picture VARCHAR(50),
+	PRIMARY KEY (profile_id)
 );
 
-
-
-
-CREATE TABLE Transactions(
-	transaction_id  INT NOT NULL identity primary key, 
-    transaction_type VARCHAR(25) NOT NULL CHECK (transaction_type IN (‘Sell’, ‘Buy’)),
-    userAccount_id INT NOT NULL FOREIGN KEY REFERENCES Users(user_id), 
-    ticker_Symbol VARCHAR(25) NOT NULL FOREIGN KEY REFERENCES Cryto(#####), 
-    submission_time DATETIME NOT NULL DEFAULT(GETDATE())
-    submission_price NUMERIC
-);
-
-CREATE TABLE Accounts(
-	user_id FOREIGN KEY REFERENCES Users(user_id),
-    account_id identity primary key,
-    account_type VARCHAR(50),
+CREATE TABLE Accounts (
+	account_id INT IDENTITY,
+    user_id VARCHAR(50) NOT NULL FOREIGN KEY REFERENCES Users(user_id),
+    account_type VARCHAR(50) NOT NULL CHECK (account_type IN ('Savings', 'Investment')),
     account_name VARCHAR(50),
-    account_balance NUMERIC
+    account_balance NUMERIC NOT NULL,
+    PRIMARY KEY (account_id)
 );
+
+CREATE TABLE Transactions (
+	transaction_id INT IDENTITY, 
+    account_id INT NOT NULL FOREIGN KEY REFERENCES Accounts(account_id),
+    transaction_type VARCHAR(25) NOT NULL CHECK (transaction_type IN ('Sell', 'Buy')),
+    ticker_symbol VARCHAR(25) NOT NULL FOREIGN KEY REFERENCES Cryto(#####), 
+    transaction_value DECIMAL(15,15) NOT NULL,
+    transaction_time DATETIME NOT NULL DEFAULT(GETDATE()),
+    crypto_rate DECIMAL(15,15) NOT NULL,
+    crypto_amount DECIMAL(15,15) NOT NULL,
+    PRIMARY KEY (transaction_id)
+);
+
+
