59034532ea6d26e429cc42d0eae63b644c669e26
Elizabeth Kang <liz.kang@hey.com>
2022-07-27 14:25:05 -0500

    Fixed DB script to include wallets
    Signed-off-by: Elizabeth Kang <liz.kang@hey.com>

--- DB.sql
+++ DB.sql
@@ -1,12 +1,3 @@
-CREATE TABLE Wallet (
-	wallet_id INT IDENTITY, 
-    PRIMARY KEY (wallet_id)
-);
-
-CREATE TABLE Crytocurrency (
-	cryptocurrency_id INT IDENTITY, 
-    PRIMARY KEY (cryptocurrency_id)
-);
 
 CREATE TABLE Users (
 	user_id INT IDENTITY,
@@ -14,36 +5,39 @@ CREATE TABLE Users (
 	password NVARCHAR(100) NOT NULL,
 	PRIMARY KEY (user_id)
 );
-
+--used during for the leaderboard and future plans-
 CREATE TABLE Profiles (
 	profile_id INT IDENTITY,
-	user_id INT NOT NULL FOREIGN KEY REFERENCES Users(user_id),
+	user_id_fk INT NOT NULL FOREIGN KEY REFERENCES Users(user_id),
 	first_name NVARCHAR(50) NOT NULL,
 	last_name NVARCHAR(50) NOT NULL,
-	display_name NVARCHAR(50) NOT NULL,
-	profile_picture NVARCHAR(50),
 	PRIMARY KEY (profile_id)
 );
 
-CREATE TABLE Accounts (
-	account_id INT IDENTITY,
-    user_id INT NOT NULL FOREIGN KEY REFERENCES Users(user_id),
-    account_type NVARCHAR(50) NOT NULL CHECK (account_type IN ('Savings', 'Investment')),
-    account_name NVARCHAR(50),
-    account_balance DECIMAL(15,15) NOT NULL,
-    PRIMARY KEY (account_id)
+CREATE TABLE Currency (
+	currency_id INT IDENTITY,
+    currency_symbol NVARCHAR(10) NOT NULL,
+    currency_current_price DECIMAL(15,15) NOT NULL,
+    currency_time DATETIME NOT NULL,
+    PRIMARY KEY (Currency_id)
+);
+
+CREATE TABLE Wallets (
+	wallet_id INT IDENTITY,
+    user_id_fk INT NOT NULL FOREIGN KEY REFERENCES Users(user_id),
+    currency_id_fk INT NOT NULL FOREIGN KEY REFERENCES Currency(currency_id),
+    amount_currency DECIMAL(15,15) NOT NULL, --must be zero, neg, or pos-
+    PRIMARY KEY (wallet_id)
 );
 
 --Still need the table for the bank/crypto setup before this table is ready to be added in db
 CREATE TABLE Transactions (
 	transaction_id INT IDENTITY, 
-    account_id INT NOT NULL FOREIGN KEY REFERENCES Accounts(account_id),
+    wallet_id_fk INT NOT NULL FOREIGN KEY REFERENCES Wallets(wallet_id),
+    ticker_symbol NVARCHAR(25) NOT NULL FOREIGN KEY REFERENCES Currency(currency_id), 
     transaction_type NVARCHAR(25) NOT NULL CHECK (transaction_type IN ('Sell', 'Buy')),
-    ticker_symbol NVARCHAR(25) NOT NULL FOREIGN KEY REFERENCES Crytocurrency(cryptocurrency_id), 
     transaction_value DECIMAL(15,15) NOT NULL,
     transaction_time DATETIME NOT NULL DEFAULT (GETDATE()),
-    crypto_rate DECIMAL(15,15) NOT NULL,
-    crypto_amount DECIMAL(15,15) NOT NULL,
     PRIMARY KEY (transaction_id)
 );
 
