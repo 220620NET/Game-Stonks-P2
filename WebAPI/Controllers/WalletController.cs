93b8569c4d64378ad32e4019f62c71c8a94e00b4
terriblea <kishanp575@gmail.com>
2022-08-01 13:41:56 -0700

    fixed build

--- WebAPI/Controllers/WalletController.cs
+++ WebAPI/Controllers/WalletController.cs
@@ -24,7 +24,7 @@ public class WalletController
         // }
         // return Results.Ok(allwallets);
         var allwallets =  await _service.GetAllWallets();
-        return allwallets.Result.Count > 0 ? Results.Ok(allwallets) : Results.NoContent();
+        return allwallets.Count > 0 ? Results.Ok(allwallets) : Results.NoContent();
 
         
     }
@@ -39,8 +39,8 @@ public class WalletController
         //     Results.BadRequest();
         // }
         // return Results.Ok(wallets);
-        Task<List<Wallet>> wallets = await _service.GetAllWalletsByUserId(user_id);
-        return wallets.Result.Count > 0 ? Results.Ok(wallets) : Results.BadRequest();
+        var wallets = await _service.GetAllWalletsByUserId(user_id);
+        return wallets.Count > 0 ? Results.Ok(wallets) : Results.BadRequest();
     }
     public async Task<IResult> CreateWallet(Wallet wallet)
     {
@@ -53,13 +53,13 @@ public class WalletController
         //     Results.BadRequest(wallet);
         // }
         // return Results.Ok(wallet);;
-        Task<bool> createdWallet = await _service.CreateWallet(wallet);
-        return createdWallet.Result == true ? Results.Ok(wallet) : Results.BadRequest();
+        var createdWallet = await _service.CreateWallet(wallet);
+        return createdWallet == true ? Results.Ok(wallet) : Results.BadRequest();
     }
     public async Task<IResult> UpdateWallet(Wallet wallet)
     {
-        Task<bool> updatedWallet = await _service.UpdateWallet(wallet);
-        return updatedWallet.Result == true ? Results.Ok(wallet) : Results.BadRequest();
+        var updatedWallet = await _service.UpdateWallet(wallet);
+        return updatedWallet == true ? Results.Ok(wallet) : Results.BadRequest();
 
     }
 }
