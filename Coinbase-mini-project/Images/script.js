cc8a2c0b4c17d90cbbffa0a6ac59988139d50194
terriblea <kishanp575@gmail.com>
2022-08-04 08:17:58 -0700

    replacing wrong script with right script?

--- Coinbase-mini-project/Images/script.js
+++ Coinbase-mini-project/Images/script.js
@@ -1,10 +1,12 @@
-function getPrice() {
-    let elem = document.getElementById('currency')
-    let currencyInput = elem.value
-    if(currencyInput){
-        fetch('https://api.coinbase.com/v2/prices/'+ currencyInput + '-USD/spot').then((response) => response.json()).then((resBody) => {
-            document.querySelector('#result-name').innerText = resBody.currency;
-            document.querySelector('result-amount').innerText = resBody.amount;
-        }).catch((err) => console.error(err));
-    }
+function getPrice(fiatInput,cryptoInput) {
+    // let fiatelem = document.getElementById('fiat');
+    // let fiatInput = fiatelem.value;
+    // let cryptoelem = document.getElementById('crypto');
+    // let cryptotInput = cryptoelem.value;
+    fetch('https://api.coinbase.com/v2/prices/'+ cryptoInput + '-'+ fiatInput +'/spot')
+    .then((response) => response.json()).then((resBody) => {
+        document.querySelector('#result-fiat').innerText = resBody.data.currency;
+        document.querySelector('#result-crypt').innerText = resBody.data.base;
+        document.querySelector('#result-amount').innerText = resBody.data.amount;
+    });
 }
