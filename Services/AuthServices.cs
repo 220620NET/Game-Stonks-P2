3bb7d2589d897580a243ac075d40847ea93637cb
noonmoon101 <kevion.chatfield@gmail.com>
2022-08-04 14:48:27 -0400

    image type change

--- Services/AuthServices.cs
+++ Services/AuthServices.cs
@@ -4,12 +4,36 @@ using CustomExceptions;
 
 namespace Services;
 
-public class AuthServices
+public class AuthServices 
 {
-    private readonly IUserDAO _user;
+    private readonly IUserDAO _userdao;
 
     public AuthServices(IUserDAO userDao)
     {
-        _user = userDao;
+        _userdao = userDao;
+    }
+    public async Task<User> Register(User findUser)
+    {
+        try
+        {
+            await _userdao.GetUserByEmail(findUser.Email);
+            throw new DuplicateRecordException();
+        }
+        catch
+        {
+            return _userdao.CreateUser(findUser);
+        }
+    }
+    public async Task<User> LogIn(User loginUser)
+    {
+        try
+        {
+            User foundUser = await _userdao.GetUserByEmail(loginUser.Email);
+            return foundUser;
+        }
+        catch(RecordNotFoundException)
+        {
+            throw new InvalidCredentialException();
+        }
     }
 }
