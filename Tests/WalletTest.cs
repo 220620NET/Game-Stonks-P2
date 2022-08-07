0b94160a13a166b72b699d8ad4b59875c7221386
terriblea <kishanp575@gmail.com>
2022-08-06 22:53:23 -0700

    First test successfully added/ moq added to dependencies

--- Tests/WalletTest.cs
+++ Tests/WalletTest.cs
@@ -1,10 +1,27 @@
+using Moq;
+using Models;
+using CustomExceptions;
+using Services;
+using Xunit;
+using DataAccess;
+using System;
+
 namespace Tests;
 
-public class UnitTest1
+public class WalletTesting
 {
     [Fact]
-    public void Test1()
+    public void GetAllWalletstest()
     {
+        var mockedRepo = new Mock<IWalletDAO>();
+
+
+        mockedRepo.Setup( repo =>  repo.GetAllWallets()).Throws(new ResourceNotFoundException());
+
+
+        WalletServices service = new WalletServices(mockedRepo.Object);
+
+        Assert.ThrowsAsync<ResourceNotFoundException>(async () => await service.GetAllWallets());
 
     }
 }
