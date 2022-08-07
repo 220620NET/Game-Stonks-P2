5842665eebdf1d9a44ad3a9388d6f506a78d6bc5
terriblea <kishanp575@gmail.com>
2022-08-07 13:23:58 -0700

    2nd test before fixes

--- Tests/WalletTest.cs
+++ Tests/WalletTest.cs
@@ -4,14 +4,13 @@ using CustomExceptions;
 using Services;
 using Xunit;
 using DataAccess;
-using System;
 
 namespace Tests;
 
 public class WalletTesting
 {
     [Fact]
-    public void GetAllWalletstest()
+    public void NoWalletsToGet()
     {
         var mockedRepo = new Mock<IWalletDAO>();
 
@@ -23,5 +22,41 @@ public class WalletTesting
 
         Assert.ThrowsAsync<ResourceNotFoundException>(async () => await service.GetAllWallets());
 
+    }
+    [Fact]
+    public void WrongWalletId()
+    {
+        var walletRepo = new Mock<IWaletDAO>();
+        var userRepo = new Mock<IUserDAO>();
+        var currencyRepo = new Mock<ICurrencyDAO>();
+
+       
+        User newUser = new User{
+            UserId = 1,
+            Email = "kishanp575@gmail.com",
+            Password = "password"
+        };
+        Currency newCurrency = new Currency{
+            CurrencyId = 1,
+            CurrencySymbol = "BTC",
+            CurrencyCurrentPrice = 10,
+            CurrencyTime = DateTime.Now
+        };
+        Wallet newWallet = new Wallet{
+            WalletId = 1,
+            UserIdFk = 1, 
+            CurrencyIdFk = 1,
+            AmountCurrency = 10
+        };
+
+        userRepo.Setup( repo => repo.createUser(newUser)).Returns(newUser);
+        currencyRepo.Setup( userRepo => repo.CreateCurrency(newCurrency)).Returns(newCurrency);
+        walletRepo.Setup( repo => repo.CreateWallet(newWallet)).Returns(newWallet);
+        walletRepo.Setup( userRepo => repo.GetAllWalletsByUserId(2)).Returns(new RecordNotFoundException());
+
+        WalletServies service = new WalletServies(walletRepo.Object);
+
+        Assert.ThrowsAsync<RecordNotFoundException>(async () => await service.GetAllWalletsByUserId(2));
+    
     }
 }
