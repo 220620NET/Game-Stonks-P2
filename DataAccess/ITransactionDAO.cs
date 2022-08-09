7d269a28bc5206f9aab9de4e26b778af743e195c
Elizabeth Kang <liz.kang@hey.com>
2022-08-08 23:46:49 -0500

    Transaction Testing
    Signed-off-by: Elizabeth Kang <liz.kang@hey.com>

--- DataAccess/ITransactionDAO.cs
+++ DataAccess/ITransactionDAO.cs
@@ -5,6 +5,7 @@ namespace DataAccess;
 public interface ITransactionDAO
 {
     Task<List<Transaction>> GetAllTransactions();
+    Task<Transaction> GetTransactionById(int id);
     Task<List<Transaction>> GetAllTransactionsByWalletId(int wallet_id);
     Task<List<Transaction>> GetAllTransactionsByCurrencyId(int currency_id);
     Task<bool> CreateTransaction(Transaction transaction);
