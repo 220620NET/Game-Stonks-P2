5af3f1ed6cb995be305013939c0c59903599893a
terriblea <kishanp575@gmail.com>
2022-08-11 10:59:03 -0700

    added formControl

--- frontEnd/src/app/register/register.component.ts
+++ frontEnd/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,6 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+  username = new FormControl('');
+  password = new FormControl('');
+  email = new FormControl('');
+
+  registerHandler = function(){
+    //register people!!!
+  }
   constructor() { }
 
   ngOnInit(): void {
