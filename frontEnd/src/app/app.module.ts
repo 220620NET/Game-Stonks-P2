b1105e75187d224b1acbc50885728392e2067dda
Elizabeth Kang <liz.kang@hey.com>
2022-08-12 22:08:53 -0500

    Working on table
    Signed-off-by: Elizabeth Kang <liz.kang@hey.com>

--- frontEnd/src/app/app.module.ts
+++ frontEnd/src/app/app.module.ts
@@ -1,17 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { AngularWebStorageModule } from 'angular-web-storage';
-
 import { SessionStorageService } from 'angular-web-storage';
+import { AppComponent } from './app.component';
+import { WalletViewComponent } from './wallet-view/wallet-view.component';
+
 @NgModule({
   declarations: [
-    
+    AppComponent,
+    WalletViewComponent
   ],
   imports: [
     BrowserModule,
