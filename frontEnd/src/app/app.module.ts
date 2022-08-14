7b98d0f342870f242efc6b16b3cd1810e3eda7c7
terriblea <kishanp575@gmail.com>
2022-08-13 22:24:05 -0700

    board working

--- frontEnd/src/app/app.module.ts
+++ frontEnd/src/app/app.module.ts
@@ -7,14 +7,11 @@ import { AppComponent } from './app.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { AngularWebStorageModule } from 'angular-web-storage';
-import { LoginComponent } from './login/login.component';
 import { SessionStorageService } from 'angular-web-storage';
 import { DashBoardComponent } from './dash-board/dash-board.component';
 @NgModule({
   declarations: [
     AppComponent,
-    RegisterComponent,
-    LoginComponent
   ],
   imports: [
     BrowserModule,
