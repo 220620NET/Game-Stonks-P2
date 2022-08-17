0d5fef7288fa79e3d1697f239e8e566527ac86b6
terriblea <kishanp575@gmail.com>
2022-08-17 02:52:04 -0700

    profile image changing works

--- frontEnd/src/app/profile/profile.component.ts
+++ frontEnd/src/app/profile/profile.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { LocalStorageService, SessionStorageService, LocalStorage, SessionStorage } from 'angular-web-storage';
+import { Profile, ProfileService } from '../profile.service';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
@@ -7,14 +8,31 @@ import { LocalStorageService, SessionStorageService, LocalStorage, SessionStorag
 })
 export class ProfileComponent implements OnInit {
 
-  constructor(private local: LocalStorageService, private session: SessionStorageService) { }
-  fname: string = '';
-  lname: string = '';
-  user: any = null;
-  get() {
-    this.user = this.session.get(this.user)
-  }
+  constructor(private local: LocalStorageService, private session: SessionStorageService,private api:ProfileService) { }
   
+  currentProfile : Profile = {
+    ProfileId: NaN,
+    UserIdFk: NaN,
+    FirstName: '',
+    LastName: ''
+  };
+  // fname: string = this.currentProfile.FirstName;
+  // lname: string = this.currentProfile.LastName;
+  // currentUser: any = null;
+  // email: string = this.currentUser.Email;
+  imageId: number = 1;
+  images: string[] =["../../assets/person-outline.svg","../../assets/sid.png",];
+
+  // get() {
+  //   this.currentUser = this.session.get(this.currentUser)
+  // }
+  // GetProfile() : void {
+  //   if (this.currentUser.UserId){
+  //     this.api.GetProfileByUserId(this.currentUser.UserId).subscribe((res) => {
+  //       this.currentProfile = res;
+  //     })
+  //   }
+  // }
   ngOnInit(): void {
   }
 
