0cd7b4b523545009af22279ca330ea49aa66b1ac
Elizabeth Kang <liz.kang@hey.com>
2022-08-17 17:31:52 -0500

    working on more buz logic

--- frontEnd/src/app/wallet.service.ts
+++ frontEnd/src/app/wallet.service.ts
@@ -14,12 +14,22 @@ export interface wallet {
 })
 export class WalletService {
 
-  apiUrl = 'https://gamestonks.azurewebsites.net/wallet'
+  apiUrl = 'https://gamestonks.azurewebsites.net/'
 
   constructor(private http: HttpClient) { }
 
   GetWallets(): Observable<wallet[]> {
-    return this.http.get<wallet[]>(this.apiUrl);
+    return this.http.get<wallet[]>(this.apiUrl + "wallet");
+  }
+
+  GetWalletsById(id: number): Observable<wallet[]> {
+    return this.http.get<wallet[]>(this.apiUrl + "wallet/" + id) as Observable<wallet[]>;
+
+  }
+
+  UpdateWalletBalance(amount: number): boolean {
+    
+    return false;
   }
 
 }
