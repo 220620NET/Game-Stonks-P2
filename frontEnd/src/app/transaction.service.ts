f0063d3dcdcfe79a94aeaf531307701aa6049a37
Elizabeth Kang <liz.kang@hey.com>
2022-08-16 19:01:15 -0500

    added transaction service

--- frontEnd/src/app/transaction.service.ts
+++ frontEnd/src/app/transaction.service.ts
@@ -1,9 +1,27 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { wallet } from './wallet.service';
+
+export interface transaction {
+  transactionId: number;
+  walletIdFk: number;
+  currencyIdFk: number;
+  transactionType: string;
+  transactionValue: number;
+  transactionTime: Date;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class TransactionService {
 
-  constructor() { }
+  apiUrl = 'https://gamestonks.azurewebsites.net/transactions'
+
+  constructor(private http: HttpClient) { }
+
+  GetTransaction(): Observable<transaction[]> {
+    return this.http.get<transaction[]>(this.apiUrl);
+  }
 }
